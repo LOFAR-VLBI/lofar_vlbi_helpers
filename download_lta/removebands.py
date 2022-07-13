9db864b76de225f04409ee91ea7cc9f556e7b6f4
jurjen93 <jurjendejong93@gmail.com>
2022-07-13 10:18:50 +0200

    test

--- download_lta/removebands.py
+++ download_lta/removebands.py
@@ -5,11 +5,11 @@ import casacore.tables as ct
 
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('--datafolder', type=str, help='path')
-parser.add_argument('--freqcut', type=float, help='input data', default=168.0)
+parser.add_argument('--freqcut', type=float, help='MHz', default=168.0)
 args = parser.parse_args()
 
 for ms in list(set(glob(args.datafolder+'/*.MS')))[::-1]:
-    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0][0]>args.freqcut:
+    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0][0]>args.freqcut*10e6:
         os.system('rm -rf ' + ms)
     else:
         continue
--- download_lta/sbatch_download.sh
+++ download_lta/sbatch_download.sh
@@ -9,3 +9,4 @@
 wget -ci $1 -P $2
 python3 /home/lofarvwf-jdejong/scripts/prefactor_helpers/download_lta/untar.py --path $2
 python3 /home/lofarvwf-jdejong/scripts/prefactor_helpers/helper_scripts/findmissingdata.py --path $2/Data
+python3 ~/scripts/prefactor_helpers/download_lta/removebands.py --datafolder $2/Data
