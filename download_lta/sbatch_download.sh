9ce1c2708bcca0ca48ee4c235bfd3df8d20ac61a
jurjen93 <jurjendejong93@gmail.com>
2022-07-13 10:22:35 +0200

    test

--- download_lta/sbatch_download.sh
+++ download_lta/sbatch_download.sh
@@ -6,7 +6,9 @@
 #1 --> html.txt
 #2 --> path
 
+SIMG=/project/lofarvwf/Software/singularity/test_lofar_sksp_v3.3.4_x86-64_generic_avx512_ddf.sif
+
 wget -ci $1 -P $2
 python3 /home/lofarvwf-jdejong/scripts/prefactor_helpers/download_lta/untar.py --path $2
 python3 /home/lofarvwf-jdejong/scripts/prefactor_helpers/helper_scripts/findmissingdata.py --path $2/Data
-python ~/scripts/prefactor_helpers/download_lta/removebands.py --datafolder $2/Data
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts ${SIMG} python ~/scripts/prefactor_helpers/download_lta/removebands.py --datafolder $2/Data
