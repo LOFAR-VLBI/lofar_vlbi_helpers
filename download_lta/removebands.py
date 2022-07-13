83b4b0f384410b758367758a07e75543e8f42a4f
jurjen93 <jurjendejong93@gmail.com>
2022-07-13 10:11:25 +0200

    test

--- download_lta/removebands.py
+++ download_lta/removebands.py
@@ -9,5 +9,5 @@ parser.add_argument('--freqcut', type=float, help='input data', default=168.0)
 args = parser.parse_args()
 
 for ms in glob(args.datafolder+'/*.MS'):
-    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0]>args.freqcut:
+    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0][0]>args.freqcut:
         os.system('rm -rf ' + ms)
