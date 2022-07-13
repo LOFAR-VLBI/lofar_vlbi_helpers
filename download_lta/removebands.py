3a47a71cc8bd3fb2070bc40b8e82565ab4495600
jurjen93 <jurjendejong93@gmail.com>
2022-07-13 10:07:34 +0200

    test

--- download_lta/removebands.py
+++ download_lta/removebands.py
@@ -5,9 +5,9 @@ import casacore.tables as ct
 
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('--datafolder', type=str, help='path')
-parser.add_argument('--freqcut', type=float, help='input data')
+parser.add_argument('--freqcut', type=float, help='input data', default=168.0)
 args = parser.parse_args()
 
 for ms in glob(args.datafolder):
-    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0]>168:
+    if ct.taql('SELECT CHAN_FREQ, CHAN_WIDTH FROM ' + ms + '::SPECTRAL_WINDOW').getcol('CHAN_FREQ')[0]>args.freqcut:
         os.system('rm -rf ' + ms)
