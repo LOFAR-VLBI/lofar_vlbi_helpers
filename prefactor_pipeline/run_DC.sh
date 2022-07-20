a07b82824bf9b066d8cf6a3009ecb0f9989701bc
jurjen93 <jurjendejong93@gmail.com>
2022-07-20 10:55:34 +0200

    test

--- prefactor_pipeline/run_DC.sh
+++ prefactor_pipeline/run_DC.sh
@@ -11,7 +11,7 @@ echo "GENERIC PIPELINE STARTING"
 export RUNDIR=$(mktemp -d -p "$TMPDIR")
 export RESULTS_DIR=$1
 export DDF_OUTPUT=$2
-export SIMG=/project/lofarvwf/Software/singularity/test_lofar_sksp_v3.3.4_x86-64_generic_avx512_ddf.sif
+export SIMG=/project/lofarvwf/Software/singularity/test_lofar_sksp_v3.3.5_cascadelake_cascadelake_avx512_cuda11_3_ddf.sif
 
 echo "RUNDIR is $(readlink -f $RUNDIR)"
 cd $RUNDIR
