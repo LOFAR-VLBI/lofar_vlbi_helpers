67adfeeb926a5c850375ef1a9b3608b7741f9630
jurjen93 <jurjendejong93@gmail.com>
2022-07-22 11:26:47 +0200

    test

--- move_data_spider/ddf_lotss.sh
+++ move_data_spider/ddf_lotss.sh
@@ -7,5 +7,5 @@ L=$1
 cp -r /project/lofarvwf/Public/jdejong/ELAIS/${L}/ddf/* /project/lotss/Public/jdejong/ELAIS/${L}/ddf/
 cp /project/lotss/Public/jdejong/scripts/ddfrun.sh /project/lotss/Public/jdejong/ELAIS/${L}/ddf/
 cp /project/lotss/Public/jdejong/scripts/pipeline.cfg /project/lotss/Public/jdejong/ELAIS/${L}/ddf/
-cd /project/lotss/Public/jdejong/ELAIS/${L}
+cd /project/lotss/Public/jdejong/ELAIS/${L}/ddf
 sbatch ./ddfrun.sh
