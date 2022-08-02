ad65eacb724b06ee03dcd7160907166e7f913ec8
jurjen93 <jurjendejong93@gmail.com>
2022-08-02 04:50:10 +0200

    test

--- move_data_spider/ddf_lotss.sh
+++ move_data_spider/ddf_lotss.sh
@@ -8,4 +8,4 @@ cp -r /project/lofarvwf/Public/jdejong/ELAIS/${L}/ddf/* /project/lotss/Public/jd
 cp /project/lotss/Public/jdejong/scripts/ddfrun.sh /project/lotss/Public/jdejong/ELAIS/${L}/ddf/
 cp /project/lotss/Public/jdejong/scripts/pipeline.cfg /project/lotss/Public/jdejong/ELAIS/${L}/ddf/
 cd /project/lotss/Public/jdejong/ELAIS/${L}/ddf
-sbatch ./ddfrun.sh
+#sbatch ./ddfrun.sh
--- move_data_spider/prefactor_to_ddf_lofarvwf.sh
+++ move_data_spider/prefactor_to_ddf_lofarvwf.sh
@@ -2,5 +2,5 @@
 
 L=$1
 
-cp -r /project/lofarvwf/Share/jdejong/output/ELAIS/${L}/target/Pre-Facet-Target/results/${L}* /project/lofarvwf/Public/jdejong/ELAIS/${L}
+cp -r /project/lofarvwf/Share/jdejong/output/ELAIS/${L}/target/Pre-Facet-Target/results/${L}*.ms /project/lofarvwf/Public/jdejong/ELAIS/${L}
 chmod -R a+rwx /project/lofarvwf/Public/jdejong/ELAIS/${L}/*
