ca0e4f868e11058fd3ced0ade50105143a66f0e0
jurjen93 <jurjendejong93@gmail.com>
2022-08-19 14:17:32 +0200

    test

--- subtract_lotss/subtract_lotss.sh
+++ subtract_lotss/subtract_lotss.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1 -c 16 --constraint=intel --job-name=subtract
 
-DELAYCAL_RESULT=$1
-
 echo "Job landed on $(hostname)"
 
+export DELAYCAL_RESULT=$1
 export RUNDIR=$PWD
+export DDF_OUTPUT=$2
 export SIMG=/project/lofarvwf/Software/singularity/test_lofar_sksp_v3.3.5_cascadelake_cascadelake_avx512_cuda11_3_ddf.sif
 
 mkdir -p Input
@@ -16,6 +16,7 @@ cp /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtract_lots
 
 sed -i "s?PREFACTOR_SCRATCH_DIR?$RUNDIR?g" pipeline.cfg
 sed -i "s?PREFACTOR_SCRATCH_DIR?$RUNDIR?g" subtract_lotss.parset
+sed -i "s?DDF_OUTPUT?$DDF_OUTPUT?g" Delay-Calibration.parset
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG genericpipeline.py -d -c pipeline.cfg subtract_lotss.parset
 
