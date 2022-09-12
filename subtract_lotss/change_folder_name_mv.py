ab7271b4ef06b287254b40bc301d99fe6fa7b8b2
jurjen93 <jurjendejong93@gmail.com>
2022-09-12 20:25:10 +0200

    test

---
+++ subtract_lotss/change_folder_name_mv.py
@@ -0,0 +1,14 @@
+"""
+This script is added to solve a bug from different DDF versions
+"""
+
+import os
+from glob import glob
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--path', type=str, help='path')
+args = parser.parse_args()
+
+for ms in glob(args.path+'/*.pre-cal.ms'):
+    os.system('mv -r '+ms+' '+ms.replace('.pre-cal',''))
