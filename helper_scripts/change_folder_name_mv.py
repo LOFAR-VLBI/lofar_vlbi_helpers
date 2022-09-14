1e22f02b8cf71285be7392cc4024cbf4a7684029
jurjen93 <jurjendejong93@gmail.com>
2022-09-14 11:17:00 +0200

    test

--- helper_scripts/change_folder_name_mv.py
+++ helper_scripts/change_folder_name_mv.py
@@ -8,7 +8,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--path', type=str, help='path')
+parser.add_argument('--suffix', type=str, help='endings from files to change')
 args = parser.parse_args()
 
-for ms in glob(args.path+'/*.pre-cal.ms'):
+for ms in glob(args.path+'/*'+args.suffix):
     os.system('mv '+ms+' '+ms.replace('.pre-cal','').replace('A1C14Et', 'A1C14Dt'))
