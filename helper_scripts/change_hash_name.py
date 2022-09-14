f22836e6568606449eb5a13aac817e21255735f4
jurjen93 <jurjendejong93@gmail.com>
2022-09-14 11:40:31 +0200

    test

--- helper_scripts/change_hash_name.py
+++ helper_scripts/change_hash_name.py
@@ -15,6 +15,6 @@ new_hash = glob(args.path+'/*.pre-cal_ddf.h5')[0].split('/')[-1].split('_')[3]
 current_hash = glob(args.path+'/*.msdpppconcat')[0].split('/')[-1].split('_')[3]
 
 for ms in glob(args.path+'/*'+args.suffix):
-    print(new_hash, current_hash)
-    print(ms)
-    # os.system('mv '+ms+' '+ms.replace(current_hash, new_hash))
+    # print(new_hash, current_hash)
+    # print(ms)
+    os.system('mv '+ms+' '+ms.replace(current_hash, new_hash))
