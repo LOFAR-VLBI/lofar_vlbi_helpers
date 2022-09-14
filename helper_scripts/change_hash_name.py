0748bdbf31e6a8ea505263ef574b37c77881040c
jurjen93 <jurjendejong93@gmail.com>
2022-09-14 11:33:17 +0200

    test

--- helper_scripts/change_hash_name.py
+++ helper_scripts/change_hash_name.py
@@ -11,5 +11,8 @@ parser.add_argument('--path', type=str, help='path')
 parser.add_argument('--suffix', type=str, help='endings from files to change')
 args = parser.parse_args()
 
+new_hash = glob(args.path+'/*.pre-cal_ddf.h5')[0].split('/')[-1].split('_')[3]
+current_hash = glob(args.path+'/*.msdpppconcat')[0].split('/')[-1].split('_')[3]
+
 for ms in glob(args.path+'/*'+args.suffix):
-    os.system('mv '+ms+' '+ms.replace('A1C14Dt', 'A1C14Et'))
+    os.system('mv '+ms+' '+ms.replace(current_hash, new_hash))
