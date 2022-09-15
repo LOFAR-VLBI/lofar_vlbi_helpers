b6b947f4676d3f35d2893136714f6236c90836a1
jurjen93 <jurjendejong93@gmail.com>
2022-09-15 10:29:55 +0200

    test

---
+++ subtract_lotss/subtraction.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -N 1 -c 32 --constraint=intel --job-name=subtract
+
+
+export SIMG=/project/lofarvwf/Software/singularity/testpatch_lofar_sksp_v3.4_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+
+
+cd subtract_lotss
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG CleanSHM.py
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG /home/lofarvwf-jdejong/scripts/lofar-highres-widefield/utils/sub-sources-outside-region.py \
+--boxfile boxfile.reg \
+--column DATA_DI_CORRECTED \
+--freqavg 1 \
+--timeavg 1 \
+--ncpu 24 \
+--prefixname sub6asec \
+--noconcat \
+--keeplongbaselines \
+--nophaseshift \
+--chunkhours 4 \
+--mslist test-mslist.txt
