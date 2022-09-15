c5d14442e37d52d7c09c8cca24f1b51a476731f2
jurjen93 <jurjendejong93@gmail.com>
2022-09-15 22:47:09 +0200

    test

--- subtract_lotss/subtraction_parallel.sh
+++ subtract_lotss/subtraction_parallel.sh
@@ -2,8 +2,8 @@
 
 RUNDIR=$1
 
-for FILE in RUNDIR/subtract_lotss/L*.ms;
-  do
-    echo "$FILE"
-    sbatch /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtraction.sh $FILE
+for FILE in $RUNDIR/subtract_lotss/L*.ms
+do
+  echo "$FILE"
+  sbatch /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtraction.sh $FILE
 done
