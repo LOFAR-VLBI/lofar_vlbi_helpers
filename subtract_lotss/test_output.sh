36942825bab06750751662ae22141549b34d2ccf
jurjen93 <jurjendejong93@gmail.com>
2022-09-16 09:58:21 +0200

    test

--- subtract_lotss/test_output.sh
+++ subtract_lotss/test_output.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1 -c 16 --job-name=test_subtract
 
-SING_IMAGE=/home/lofarvwf-jdejong/singularities/lofar_sksp_fedora31_ddf_fixed.sif
-SING_BIND=/project/lofarvwf/Share/jdejong
+export SIMG=/project/lofarvwf/Software/singularity/testpatch_lofar_sksp_v3.4_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
 
 MS_IN=$1
 
 #singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${MS_IN} msout=avg_${MS_IN} steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
 #singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=sub6asec_${MS_IN} msout=avg_sub6asec_${MS_IN} steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
 
-wsclean \
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG wsclean \
 -no-update-model-required \
 -minuv-l 80 \
 -size 8192 8192 \
@@ -29,7 +28,7 @@ wsclean \
 -maxuv-l 20e3 \
 avg_${MS_IN}
 
-wsclean \
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG wsclean \
 -no-update-model-required \
 -minuv-l 80 \
 -size 8192 8192 \
