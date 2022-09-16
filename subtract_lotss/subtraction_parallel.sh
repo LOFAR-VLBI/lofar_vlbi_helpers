19f07a3761614ee6934ee7545b722db9c6a906d3
jurjen93 <jurjendejong93@gmail.com>
2022-09-16 15:20:47 +0200

    test

--- subtract_lotss/subtraction_parallel.sh
+++ subtract_lotss/subtraction_parallel.sh
@@ -16,18 +16,18 @@ cd ${DIR}
 for FILE in ${OBSERVATION}*.ms
 do
   echo "${FILE}"
-  mkdir -p ${FILE}_suboutput
-  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.DicoModel ${FILE}_suboutput
-  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.mask01.fits ${FILE}_suboutput
-  cp ${DDF_OUTPUT}/image_dirin_SSD_m.npy.ClusterCat.npy ${FILE}_suboutput
-  cp ${DDF_OUTPUT}/DDS3_full_*_merged.npz ${FILE}_suboutput
-  cp ${DDF_OUTPUT}/DDS3_full_*_smoothed.npz ${FILE}_suboutput
-  cp boxfile.reg ${FILE}_suboutput
-  cp cutoutmask.fits ${FILE}_suboutput
+#  mkdir -p ${FILE}_suboutput
+#  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.DicoModel ${FILE}_suboutput
+#  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.mask01.fits ${FILE}_suboutput
+#  cp ${DDF_OUTPUT}/image_dirin_SSD_m.npy.ClusterCat.npy ${FILE}_suboutput
+#  cp ${DDF_OUTPUT}/DDS3_full_*_merged.npz ${FILE}_suboutput
+#  cp ${DDF_OUTPUT}/DDS3_full_*_smoothed.npz ${FILE}_suboutput
+#  cp boxfile.reg ${FILE}_suboutput
+#  cp cutoutmask.fits ${FILE}_suboutput
   cp -r ${DDF_OUTPUT}/SOLSDIR ${FILE}_suboutput
-  mv ${FILE} ${FILE}_suboutput
-  cd ${FILE}_suboutput
-  echo ${FILE} > mslist.txt
+#  mv ${FILE} ${FILE}_suboutput
+#  cd ${FILE}_suboutput
+#  echo ${FILE} > mslist.txt
   sbatch /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtraction.sh mslist.txt
   cd ../
 done
