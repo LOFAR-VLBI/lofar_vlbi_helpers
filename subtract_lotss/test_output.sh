aff1093be06eb854d984f9f36d011a8dd3ee4f71
jurjen93 <jurjendejong93@gmail.com>
2022-09-16 10:28:01 +0200

    test

--- subtract_lotss/test_output.sh
+++ subtract_lotss/test_output.sh
@@ -8,27 +8,31 @@ MS_IN=$1
 #singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=${MS_IN} msout=${MS_IN}_avg steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
 #singularity exec -B ${SING_BIND} ${SING_IMAGE} DPPP msin=sub6asec_${MS_IN}* msout=sub6asec_${MS_IN}_avg steps=[av] msout.storagemanager=dysco steps=[av] av.type=averager av.freqstep=16 av.timestep=16
 
-singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG wsclean \
--scale 3arcsec \
--taper-gaussian 20arcsec \
--no-update-model-required \
--minuv-l 80 \
--size 8192 8192 \
--reorder \
--weight briggs -0.5 \
--cleanborder 1 \
--parallel-reordering 4 \
--mgain 0.75 \
--data-column DATA \
--auto-mask 2.5 \
--auto-threshold 0.5 \
--pol i -use-wgridder \
--name testimage \
--scale 3arcsec \
--nmiter 10 \
--niter 100000 \
--maxuv-l 20e3 \
-avg_${MS_IN}
+#singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG wsclean \
+#-scale 3arcsec \
+#-taper-gaussian 20arcsec \
+#-no-update-model-required \
+#-minuv-l 80 \
+#-size 8192 8192 \
+#-reorder \
+#-weight briggs -0.5 \
+#-cleanborder 1 \
+#-parallel-reordering 4 \
+#-mgain 0.75 \
+#-data-column DATA \
+#-auto-mask 2.5 \
+#-auto-threshold 0.5 \
+#-pol i -use-wgridder \
+#-name testimage \
+#-scale 3arcsec \
+#-nmiter 10 \
+#-niter 100000 \
+#-maxuv-l 20e3 \
+#avg_${MS_IN}
+#
+#mkdir test_sub6asec
+#mv avg_sub6asec_${MS_IN}_avg.sub.shift.avg.ms test_sub6asec
+#cd test_sub6asec
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG wsclean \
 -scale 3arcsec \
