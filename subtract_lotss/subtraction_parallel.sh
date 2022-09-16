8e261c0f28a94005d33fe6bb3fb01673fc84e197
jurjen93 <jurjendejong93@gmail.com>
2022-09-16 13:37:58 +0200

    bug

--- subtract_lotss/subtraction_parallel.sh
+++ subtract_lotss/subtraction_parallel.sh
@@ -6,7 +6,7 @@ mkdir -p subtract_lotss
 DIR=subtract_lotss/
 
 re="L[0-9][0-9][0-9][0-9][0-9][0-9]"
-if [[ $RUNDIR =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
+if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
 echo echo "SUBTRACT START ${OBSERVATION}"
 
 DDF_OUTPUT=/project/lotss/Public/jdejong/ELAIS/${OBSERVATION}/ddf/
