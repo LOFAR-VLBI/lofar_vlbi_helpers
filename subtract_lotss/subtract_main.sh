a51df611b0c722b6b406849763de4c2ee904d061
jurjen93 <jurjendejong93@gmail.com>
2022-09-17 11:03:32 +0200

    test

--- subtract_lotss/subtract_main.sh
+++ subtract_lotss/subtract_main.sh
@@ -13,14 +13,14 @@ mkdir -p Input
 cp -r ${DELAYCAL_RESULT}/L*.msdpppconcat Input
 
 cp /home/lofarvwf-jdejong/scripts/prefactor_helpers/lofar-vlbi-setup/pipeline.cfg .
-cp /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtract_lotss_setup.parset .
+cp /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtract_lotss.parset .
 
 sed -i "s?PREFACTOR_SCRATCH_DIR?$RUNDIR?g" pipeline.cfg
-sed -i "s?PREFACTOR_SCRATCH_DIR?$RUNDIR?g" subtract_lotss_setup.parset
-sed -i "s?DDF_OUTPUT?$DDF_OUTPUT?g" subtract_lotss_setup.parset
+sed -i "s?PREFACTOR_SCRATCH_DIR?$RUNDIR?g" subtract_lotss.parset
+sed -i "s?DDF_OUTPUT?$DDF_OUTPUT?g" subtract_lotss.parset
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG CleanSHM.py
-singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG genericpipeline.py -d -c pipeline.cfg subtract_lotss_setup.parset
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG genericpipeline.py -d -c pipeline.cfg subtract_lotss.parset
 
 echo "... done"
 echo "SUBTRACT SETUP FINISHED"
