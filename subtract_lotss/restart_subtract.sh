81bd5a40b9a31287fba4fb7d077fb154f08a1ba0
jurjen93 <jurjendejong93@gmail.com>
2022-09-19 20:10:40 +0200

    test

---
+++ subtract_lotss/restart_subtract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1 -c 4 --job-name=subtract_distribute
+
+echo "Job landed on $(hostname)"
+
+re="L[0-9][0-9][0-9][0-9][0-9][0-9]"
+re_subband="([^.]+)"
+if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
+echo "SUBTRACT START ${OBSERVATION}"
+
+cd subtract_lotss
+
+for FILE in ${OBSERVATION}*.msdpppconcat_subrun
+do
+  echo "Subtract ${FILE}"
+  if [[ ${FILE} =~ $re_subband ]]; then SUBBAND=${BASH_REMATCH}; fi
+  mv ${FILE} ${SUBBAND}_subrun
+  mv ${SUBBAND}_subrun/${SUBBAND}.msdpppconcat ${SUBBAND}_subrun/${SUBBAND}.pre-cal.ms
+  cd ${SUBBAND}_subrun
+  echo ${FILE} > mslist.txt
+  rm -rf SOLSDIR/*.msdpppconcat
+  sbatch /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtraction.sh mslist.txt
+  cd ../
+done
--- subtract_lotss/subtraction_parallel.sh
+++ subtract_lotss/subtraction_parallel.sh
@@ -4,6 +4,7 @@
 echo "Job landed on $(hostname)"
 
 re="L[0-9][0-9][0-9][0-9][0-9][0-9]"
+re_subband="([^.]+)"
 if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
 echo echo "SUBTRACT START ${OBSERVATION}"
 
@@ -19,17 +20,18 @@ mv /project/lofarvwf/Share/jdejong/output/ELAIS/${OBSERVATION}/subtract/Input/*m
 for FILE in ${OBSERVATION}*.msdpppconcat
 do
   echo "Subtract ${FILE}"
-  mkdir -p ${FILE}_subrun
-  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.DicoModel ${FILE}_subrun
-  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.mask01.fits ${FILE}_subrun
-  cp ${DDF_OUTPUT}/image_dirin_SSD_m.npy.ClusterCat.npy ${FILE}_subrun
-  cp ${DDF_OUTPUT}/DDS3_full_*_merged.npz ${FILE}_subrun
-  cp ${DDF_OUTPUT}/DDS3_full_*_smoothed.npz ${FILE}_subrun
-  cp /project/lofarvwf/Share/jdejong/output/ELAIS/${OBSERVATION}/subtract/boxfile.reg ${FILE}_subrun
-  #cp cutoutmask.fits ${FILE}_subrun
-  cp -r ${DDF_OUTPUT}/SOLSDIR ${FILE}_subrun
-  mv ${FILE} ${FILE}_subrun
-  cd ${FILE}_subrun
+  if [[ $PWD =~ $re ]]; then SUBBAND=${BASH_REMATCH}; fi
+  mkdir -p ${SUBBAND}_subrun
+  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.DicoModel ${SUBBAND}_subrun
+  cp ${DDF_OUTPUT}/image_full_ampphase_di_m.NS.mask01.fits ${SUBBAND}_subrun
+  cp ${DDF_OUTPUT}/image_dirin_SSD_m.npy.ClusterCat.npy ${SUBBAND}_subrun
+  cp ${DDF_OUTPUT}/DDS3_full_*_merged.npz ${SUBBAND}_subrun
+  cp ${DDF_OUTPUT}/DDS3_full_*_smoothed.npz ${SUBBAND}_subrun
+  cp /project/lofarvwf/Share/jdejong/output/ELAIS/${OBSERVATION}/subtract/boxfile.reg ${SUBBAND}_subrun
+  #cp cutoutmask.fits ${SUBBAND}_subrun
+  cp -r ${DDF_OUTPUT}/SOLSDIR ${SUBBAND}_subrun
+  mv ${FILE} ${SUBBAND}_subrun/${SUBBAND}.pre-cal.ms
+  cd ${SUBBAND}_subrun
   echo ${FILE} > mslist.txt
   sbatch /home/lofarvwf-jdejong/scripts/prefactor_helpers/subtract_lotss/subtraction.sh mslist.txt
   cd ../
