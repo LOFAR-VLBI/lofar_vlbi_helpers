01bc2a22264062eb63316b2771f599382aa21195
jurjen93 <jurjendejong93@gmail.com>
2022-09-21 15:02:28 +0200

    test

--- subtract_lotss/make_subtract_images.sh
+++ subtract_lotss/make_subtract_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -N 1 -c 32 --job-name=test_image --exclusive --constraint=napels
+#SBATCH -N 1 -c 32 --job-name=test_image --exclusive --constraint=naples
 
 #re="L[0-9][0-9][0-9][0-9][0-9][0-9]"
 #re_subband="([^.]+)"
