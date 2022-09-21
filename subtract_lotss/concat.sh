b55ef52f38f44d5cd3e55f8a8af7bd346bb80b87
jurjen93 <jurjendejong93@gmail.com>
2022-09-21 14:28:12 +0200

    test

--- subtract_lotss/concat.sh
+++ subtract_lotss/concat.sh
@@ -4,7 +4,7 @@
 export SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.4_x86-64_generic_noavx512_ddf.sif
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG DP3 \
-msin=sub6asec_L*.ms \
+msin=mslist.txt \
 msin.datacolumn=DATA \
 msout=TargetName_120_168MHz_averaged.ms \
 msout.storagemanager=dysco \
@@ -12,6 +12,6 @@ msout.writefullresflag=False \
 steps=[ps,avg] \
 ps.type=phaseshifter \
 ps.phasecenter=[16h06m07.61855,55d21m35.4166] \
-avg.type=averager
+avg.type=averager \
 avg.freqstep=8 \
 avg.timestep=4
