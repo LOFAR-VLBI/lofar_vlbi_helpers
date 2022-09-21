1a82353fe924ada6cf39236630559e7621c2d499
jurjen93 <jurjendejong93@gmail.com>
2022-09-21 14:11:58 +0200

    test

--- subtract_lotss/concat.sh
+++ subtract_lotss/concat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -N 1 -c 4 --job-name=subtract_main --constraint=intel
+#SBATCH -N 1 -c 4 --job-name=subtract_main
 
-export SIMG=/project/lofarvwf/Software/singularity/testpatch_lofar_sksp_v3.4_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+export SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.4_x86-64_generic_noavx512_ddf.sif
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG DPPP \
 msin=sub6asec_L*.ms \
@@ -9,9 +9,8 @@ msin.datacolumn=DATA \
 msout=TargetName_120_168MHz_averaged.ms \
 msout.storagemanager=dysco \
 msout.writefullresflag=False \
-steps=[ps, av] \
-ps.type=phaseshift \
-ps.phasecenter=[16h06m07.61855,55d21m35.4166] \
-av.type=averager \
-av.freqstep=8 \
-av.timestep=4
+steps=[phaseshift, averager] \
+phaseshift.type=phaseshift \
+phaseshift.phasecenter=[16h06m07.61855,55d21m35.4166] \
+averager.freqstep=8 \
+averager.timestep=4
