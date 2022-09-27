45c907640280d9fda933ed94461626df9a5bd184
jurjen93 <jurjendejong93@gmail.com>
2022-09-27 16:01:23 +0200

    test

--- cleanup/clean_calibrator.sh
+++ cleanup/clean_calibrator.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm -rf Data
 rm -rf Pre-Facet-Calibrator/*prep_cal
--- cleanup/clean_target.sh
+++ cleanup/clean_target.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -rf Data
 rm -rf Pre-Facet-Target/*prep_target
 rm -rf Pre-Facet-Target/*.msdpppconcat
 rm Pre-Facet-Target/*msdpppconcat.h5
