e2f73f1c6af936f1d4a7700c3410fda22c1767b9
jurjen93 <jurjendejong93@gmail.com>
2022-10-24 17:18:18 +0200

    applycal

--- applycal/applycal.sh
+++ applycal/applycal.sh
@@ -7,7 +7,7 @@ H5=$2
 SIMG=/home/lofarvwf-jdejong/singularities/lofar_sksp_fedora31_ddf_fixed.sif
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG \
-python /home/lofarvwf-jdejong/scripts/prefactor_helpers/delayselfcal/applycal.py \
+python /home/lofarvwf-jdejong/scripts/prefactor_helpers/applycal/applycal.py \
 --msin ${MSIN} \
 --msout applycal_${MSIN} \
 --h5 ${H5}
