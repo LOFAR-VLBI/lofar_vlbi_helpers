a9c5fd10d2d3ba8372fbd634dd62f4ad2e393d33
jurjen93 <jurjendejong93@gmail.com>
2022-10-24 18:01:41 +0200

    test

--- imaging/wsclean_1asec.sh
+++ imaging/wsclean_1asec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -c 48
+#SBATCH -c 31
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=jurjendejong@strw.leidenuniv.nl
 
@@ -15,46 +15,49 @@ cp -r /project/lofarvwf/Share/jdejong/output/LB_test_data/$${MSIN} "$TMPDIR"/wsc
 #cp /project/lofarvwf/Share/jdejong/output/LB_test_data/merged_testpython3_withCS_jan22.h5 "$TMPDIR"/wscleandata
 cd "$TMPDIR"/wscleandata
 
+MEM=$((100/$1))
+
 singularity exec -B ${SING_BIND} ${SING_IMAGE_WSCLEAN} \
 wsclean \
+
 -update-model-required \
 -temp-dir "$TMPDIR"/wscleandata \
 -use-wgridder \
 -minuv-l 80.0 \
--size 20000 20000 \
+-size 22500 22500 \
 -weighting-rank-filter 3 \
 -reorder \
 -weight briggs -1.5 \
 -parallel-reordering 6 \
 -mgain 0.65 \
--data-column DATA \
--auto-mask 2.5 \
+-data-column CORRECTED_DATA \
+-auto-mask 3 \
 -auto-threshold 1.0 \
 -pol i \
--name image_test \
+-name 1.2asec_image \
 -scale 0.4arcsec \
--taper-gaussian 1asec \
+-taper-gaussian 1.2asec \
 -niter 50000 \
 -log-time \
--multiscale-scale-bias 0.7 \
+-multiscale-scale-bias 0.6 \
 -parallel-deconvolution 2600 \
 -multiscale \
 -multiscale-max-scales 9 \
 -nmiter 1 \
--parallel-gridding $1 \
--mem ${MEM} \
+-parallel-gridding 10 \
+-mem 10 \
 -apply-facet-beam \
 -facet-beam-update 600 \
 -use-differential-lofar-beam \
--channels-out 3 \
+-channels-out 1 \
 -deconvolution-channels 3 \
 -join-channels \
--j $2 \
+-j ${SLURM_CPUS_PER_TASK} \
 ${MSIN}
 
-#rm -rf sub6asec_L686962_SB001_uv_12CFFDDA8t_1*
-#rm merged_testpython3_withCS_jan22.h5
-#tar cf output.tar *MFS*.fits
-#cp "$TMPDIR"/wscleandata/output.tar $1
+rm -rf sub6asec_L686962_SB001_uv_12CFFDDA8t_1*
+rm merged_testpython3_withCS_jan22.h5
+tar cf output.tar *MFS*.fits
+cp "$TMPDIR"/wscleandata/output.tar $1
 
 echo "----FINISHED----"
