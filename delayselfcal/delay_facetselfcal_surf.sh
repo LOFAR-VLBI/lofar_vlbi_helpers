9daed4cc1ccc6afbff00d13566c22e762cd7a821
jurjen93 <jurjendejong93@gmail.com>
2022-10-25 16:49:01 +0200

    test

--- delayselfcal/delay_facetselfcal_surf.sh
+++ delayselfcal/delay_facetselfcal_surf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1 -c 24 --job-name=delayselfcal
 
-SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.4_x86-64_generic_noavx512_ddf.sif
+SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_ddf_rijnX.sif
 BIND=$PWD,/project,/home/lofarvwf-jdejong/scripts
 
 #INPUT CONCATTENATED MS FILE
