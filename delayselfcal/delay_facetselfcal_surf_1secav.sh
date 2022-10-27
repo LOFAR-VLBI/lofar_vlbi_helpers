4d5a8384e005c21e79ca4b996832d7f6aa8ca3c4
jurjen93 <jurjendejong93@gmail.com>
2022-10-27 10:12:39 +0200

    test

--- delayselfcal/delay_facetselfcal_surf_1secav.sh
+++ delayselfcal/delay_facetselfcal_surf_1secav.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1 -c 24 --job-name=delayselfcal
 
+re="L[0-9][0-9][0-9][0-9][0-9][0-9]"
+if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
+
 SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.4_x86-64_generic_noavx512_ddf.sif
 BIND=$PWD,/project,/home/lofarvwf-jdejong/scripts
 
 #INPUT CONCATTENATED MS FILE
 MSIN=$1
+
+cp -r /project/lofarvwf/Share/jdejong/output/ELAIS/${OBSERVATION}/subtract/subtract_lotss/${OBSERVATION}_120_168MHz_averaged.ms .
 
 singularity exec -B $BIND $SIMG \
 /home/lofarvwf-jdejong/scripts/prefactor_helpers/delayselfcal/delaycal_1secav.sh ${MSIN}
