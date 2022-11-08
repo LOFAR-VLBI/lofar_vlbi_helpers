e4d61d6e6e23aeb05802cfd698c0142d5116dc39
jurjen93 <jurjendejong93@gmail.com>
2022-11-08 11:49:08 +0100

    test

--- split_directions/split_directions.sh
+++ split_directions/split_directions.sh
@@ -23,10 +23,16 @@ while read -r LNUM; do
   H5=/project/lofarvwf/Share/jdejong/output/ELAIS/${LNUM}/delayselfcal/merged_selfcalcyle000_linearfulljones_${LNUM}_120_168MHz_averaged.ms.avg.h5
   cp ${H5} .
   if [[ "$LNUM" =~ ^(L798074|L816272|)$ ]]; then
+
       #make calibrator parsets
-      singularity exec -B $PWD,/project $SIMG python ${SCRIPTS}/split_directions/make_calibrator_parsets.py --catalog ${CATALOG} --already_averaged_data --prefix ${LNUM}
+
+      pattern="../subtract/subtract_lotss/sub6asec_L816272_SB000_uv_131A1C14Et_1*"
+      files=( $pattern )
+      first_file=${files[0]}
+
+      singularity exec -B $PWD,/project $SIMG python ${SCRIPTS}/split_directions/make_calibrator_parsets.py --catalog ${CATALOG} --already_averaged_data --prefix ${LNUM} --ms ${first_file}
   else
-      singularity exec -B $PWD,/project $SIMG python ${SCRIPTS}/split_directions/make_calibrator_parsets.py --catalog ${CATALOG} --h5 ${H5} ${LNUM}
+      singularity exec -B $PWD,/project $SIMG python ${SCRIPTS}/split_directions/make_calibrator_parsets.py --catalog ${CATALOG} --h5 ${H5} ${LNUM} --ms ${first_file}
   fi
   echo "Made parsets for ${LNUM}"
   for P in ${LNUM}*.parset; do
