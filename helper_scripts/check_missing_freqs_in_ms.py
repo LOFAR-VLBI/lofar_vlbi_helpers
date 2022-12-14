924b370ec1c390516390dc25fca09a7b00e37fac
jurjen93 <jurjendejong93@gmail.com>
2022-12-14 14:40:24 +0100

    update

--- helper_scripts/check_missing_freqs_in_ms.py
+++ helper_scripts/check_missing_freqs_in_ms.py
@@ -10,18 +10,19 @@ import numpy as np
 import sys
 from glob import glob
 
+
 def get_channels(input):
     """
     :param input: list or string with measurement sets
     :return: sorted concatenated channels
     """
-    if type(input)==str:
+    if type(input) == str:
         input = glob(input)
 
     for n, ms in enumerate(input):
-        t = ct.table(ms+'/::SPECTRAL_WINDOW')
+        t = ct.table(ms + '/::SPECTRAL_WINDOW')
 
-        if n==0:
+        if n == 0:
             chans = t.getcol("CHAN_FREQ")
         else:
             chans = np.append(chans, t.getcol("CHAN_FREQ"))
@@ -30,7 +31,8 @@ def get_channels(input):
 
     return np.sort(chans)
 
-def check_channels(input):
+
+def check_channels(input, make_dummies):
     """
     :param input: list or string with measurement sets
     :return: True if no error, otherwise sys.exit
@@ -39,17 +41,28 @@ def check_channels(input):
     chan_diff = np.abs(np.diff(chans, n=2))
     # check gaps in freqs
     if np.sum(chan_diff) != 0:
-        sys.exit("ERROR: there are channel gaps.")
+        if not make_dummies:
+            sys.exit("ERROR: there are channel gaps.")
+        else:
+            dummy_num = np.sum(chan_diff > 0) // 2
+            file = open('mslist.txt', 'a')
+            for n in range(dummy_num):
+                print('dummy_' + str(n) + ' added to mslist.txt')
+                file.write('dummy_'+str(n)+'\n')
+            file.close()
+            sys.exit("WARNING: there is/are " + str(dummy_num) + " gaps.")
 
     return True
+
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Validate MS')
     parser.add_argument('--ms', nargs='+', help='MS', required=True)
+    parser.add_argument('--make_dummies', help='Make dummies for missing MS', action='store_true')
 
     args = parser.parse_args()
 
-    if check_channels(args.ms):
-        print('--- Gaps checked and no freq gaps ---')
+    if check_channels(input=args.ms, make_dummies=args.make_dummies):
+        print('--- SUCCESS: freq gaps checked and no gaps found ---')
--- subtract_lotss/concat.sh
+++ subtract_lotss/concat.sh
@@ -6,10 +6,16 @@ if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
 
 export SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.4_x86-64_generic_noavx512_ddf.sif
 
-singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG python /home/lofarvwf-jdejong/scripts/prefactor_helpers/helper_scripts/
+ls sub6asec*.ms -1d > "mslist.txt"
+
+# check input
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG \
+python /home/lofarvwf-jdejong/scripts/prefactor_helpers/helper_scripts/check_missing_freqs_in_ms.py --ms sub6asec*.ms --make_dummies
 
 singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG DP3 \
-msin=sub6asec*.ms \
+msin=mslist.txt \
+msin.orderms=False \
+msin.missingdata=True \
 msin.datacolumn=DATA \
 msout=${OBSERVATION}_120_168MHz_averaged.ms \
 msout.storagemanager=dysco \
@@ -20,3 +26,7 @@ ps.phasecenter=[16h06m07.61855,55d21m35.4166] \
 avg.type=averager \
 avg.freqstep=8 \
 avg.timestep=4
+
+# check output
+singularity exec -B $PWD,/project,/home/lofarvwf-jdejong/scripts $SIMG \
+python /home/lofarvwf-jdejong/scripts/prefactor_helpers/helper_scripts/check_missing_freqs_in_ms.py --ms ${OBSERVATION}_120_168MHz_averaged.ms
