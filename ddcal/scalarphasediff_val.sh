2ee5312791835f078559909aa45e5c8a0c5b9f11
jurjen93 <jurjendejong93@gmail.com>
2022-12-14 15:26:53 +0100

    scalarphasediff

---
+++ ddcal/scalarphasediff_val.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -c 6
+#SBATCH --job-name=selfcal
+#SBATCH --constraint=intel
+
+#SINGULARITY SETTINGS
+BIND=$PWD,/project,/home/lofarvwf-jdejong/scripts
+SIMG=/home/lofarvwf-jdejong/singularities/lofar_sksp_v4.0.1_x86-64_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+
+MSIN=$1
+
+singularity exec -B $BIND $SIMG python  facetselfcal.py -i scalarphasediff \
+--forwidefield \
+--phaseupstations='core' \
+--msinnchan=120 \
+--avgfreqstep=2 \
+--skipbackup \
+--uvmin=20000 \
+--soltype-list="['scalarphasediff']" \
+--solint-list="['10min']" \
+--nchan-list="[6]" \
+--docircular \
+--uvminscalarphasediff=0 \
+--stop=2 \
+--soltypecycles-list="[0]" \
+--imsize=1600 \
+--skymodelpointsource=1.0 \
+--stopafterskysolve ${MSIN}
