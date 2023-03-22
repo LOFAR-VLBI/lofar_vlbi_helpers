814c2a5cd68fd4d3eb0ccb503d5a9bb4deb64c84
jurjen93 <jurjendejong93@gmail.com>
2023-03-22 15:16:28 +0100

    test

--- split_directions/make_directions_parsets.py
+++ split_directions/make_directions_parsets.py
@@ -52,7 +52,7 @@ def find_candidates(cat, ms, fluxcut=25e-3, extra_candidates=[]):
     tab.rename_column('DEC', 'DEC')
 
     # In case of multiple components of a single source being found, calculate the mean position.
-    candidates = Table(names=['Source_id', 'RA', 'DEC'], dtype=[str, float, float])
+    candidates = Table(names=['Source_id', 'RA', 'DEC'])
 
     # Get phase dir of observation
     t = ct.table(ms+'::FIELD')
