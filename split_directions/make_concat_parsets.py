03a6fa9b4ff60bea9d5ea28b20ec7c04837baed8
jurjen93 <jurjendejong93@gmail.com>
2023-04-03 11:05:59 +0200

    test

--- split_directions/make_concat_parsets.py
+++ split_directions/make_concat_parsets.py
@@ -11,19 +11,23 @@ all_dirs = list(set([f.split('_')[2].split('.')[0] for f in all_ms if 'P' in f])
 
 for observation in all_obs:
     for dir in all_dirs:
-        mslist = glob(observation + '*' + dir + '.ms')
-        mschain = ' '.join(mslist)
-        txt_name = observation + '_' + dir + '.txt'
-        os.system(f'python /home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/extra_scripts/check_missing_freqs_in_ms.py --ms {mschain} --make_dummies --output_name {txt_name}')
-        with open(txt_name) as f:
-            lines = f.readlines()
-        parset = 'msin=' + '['+', '.join(lines).replace('\n', '')+']\n'
-        parset += 'msout=' + observation + '_' + dir + '.ms'
-        parset += '\nmsin.datacolumn=DATA' \
-                  '\nmsin.missingdata=True' \
-                  '\nmsin.orderms=False' \
-                  '\nmsout.storagemanager=dysco' \
-                  '\nmsout.writefullresflag=False' \
-                  '\nsteps=[]'
-        with open(observation + '_' + dir + '.parset', 'w') as f:
-            f.write(parset)
+        try:
+            mslist = glob(observation + '*' + dir + '.ms')
+            mschain = ' '.join(mslist)
+            txt_name = observation + '_' + dir + '.txt'
+            os.system(f'python /home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/extra_scripts/check_missing_freqs_in_ms.py --ms {mschain} --make_dummies --output_name {txt_name}')
+            with open(txt_name) as f:
+                lines = f.readlines()
+            parset = 'msin=' + '['+', '.join(lines).replace('\n', '')+']\n'
+            parset += 'msout=' + observation + '_' + dir + '.ms'
+            parset += '\nmsin.datacolumn=DATA' \
+                      '\nmsin.missingdata=True' \
+                      '\nmsin.orderms=False' \
+                      '\nmsout.storagemanager=dysco' \
+                      '\nmsout.writefullresflag=False' \
+                      '\nsteps=[]'
+            with open(observation + '_' + dir + '.parset', 'w') as f:
+                f.write(parset)
+        except FileNotFoundError:
+            with open('failed_concat.txt', 'a+') as f:
+                f.write(observation+'_'+dir+'\n')
