902285522cd14c52a1f16fd3bf7559ccc7461e05
jurjen93 <jurjendejong93@gmail.com>
2023-05-06 11:36:27 +0200

    update

--- analyse/find_solint.py
+++ analyse/find_solint.py
@@ -1,6 +1,17 @@
+import tables
+from glob import glob
+import numpy as np
+import csv
+from scipy.stats import circstd, circmean
+import sys
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import normal
+import scienceplots
+from scipy.stats import circstd, circvar
+import random
+import math
 
 # pretty plots
 try:
@@ -9,103 +20,159 @@ try:
 except:
     pass
 
-
-# set std score, for which you want to find the solint
-optimal_score = 0.5
-
-def get_circvar(phasediff):
+def make_utf8(inp):
     """
-    Get circular variance
+    Convert input to utf8 instead of bytes
 
-    From: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.circstd.html
-
-    :param phasediff: phase difference between XX and YY
+    :param inp: string input
+    :return: input in utf-8 format
     """
-    A = np.cos(phasediff).mean()
-    B = np.sin(phasediff).mean()
-    R = np.sqrt(A ** 2 + B ** 2)
-    return -2 * np.log(R)
+
+    try:
+        inp = inp.decode('utf8')
+        return inp
+    except (UnicodeDecodeError, AttributeError):
+        return inp
+
+class GetSolint:
+    def __init__(self, h5, optimal_score=0.5, ref_solint=10):
+        """
+        Get a score based on the phase difference between XX and YY. This reflects the noise in the observation.
+        From this score we can determine an optimal solution interval, by fitting a wrapped normal distribution.
+
+        See:
+        - https://en.wikipedia.org/wiki/Wrapped_normal_distribution
+        - https://en.wikipedia.org/wiki/Yamartino_method
+        - https://en.wikipedia.org/wiki/Directional_statistics
+
+        :param h5: h5parm
+        :param optimal_score: score to fit solution interval
+        :param ref_solint: reference solution interval
+        """
+        self.h5 = h5
+        self.optimal_score = optimal_score
+        self.ref_solint = ref_solint
+
+    def plot_C(self, title=None, saveas=None):
+        """
+        Plot circstd score in function of solint for given C
+
+        :param C: constant defining the noise level
+        :param title: title for plot
+        """
+        normal_sigmas = [n / 10 for n in range(0, 100)]
+        values = [circstd(normal(0, n, 10000)) for n in normal_sigmas]
+        x = self.C / (np.array(normal_sigmas) ** 2) * self.ref_solint
+        bestsolint = self.C / (self._circvar_to_normvar(self.optimal_score ** 2)) * self.ref_solint
+        plt.plot(x, values)
+        plt.scatter([bestsolint], [self.optimal_score], color='red')
+        plt.xlim(0, max(bestsolint * 2, 2.1))
+        plt.xlabel("solint (min)")
+        plt.ylabel("circstd score")
+        if title is not None:
+            plt.title(title)
+        if saveas is not None:
+            plt.savefig(saveas)
+        else:
+            plt.show()
+
+    def _circvar_to_normvar(self, circ_var):
+        """
+        Convert circular variance to normal variance
+
+        return: circular variance
+        """
+        # normalized circular variance
+        circ_varnorm = circ_var / np.pi
+
+        if circ_varnorm >= 1:
+            return np.pi
+        else:
+            return -2 * np.log(1 - circ_varnorm)
 
 
-def get_circstd(phasediff):
-    """
-    Get circular std
-    """
-    return np.sqrt(get_circvar(phasediff))
+    def _get_C(self, cstd):
+        """
+        Get constant defining the normal circular distribution
+
+        :param cstd: circular standard deviation
+
+        :return: C
+        """
+        normvar = self._circvar_to_normvar(cstd ** 2)
+        solint = 1  # by setting it to 1, the function returns how many times solution interval increase
+        return normvar * solint
 
 
-def plot_C(C, title=None):
-    """
-    Plot circstd score in function of solint for given C
+    def get_phasediff_score(self, h5, station=False):
+        """
+        Calculate score for phasediff
 
-    :param C: constant defining the noise level
-    """
-    normal_sigmas = [n / 10 for n in range(0, 100)]
-    values = [get_circstd(normal(0, n, 10000)) for n in normal_sigmas]
-    x = C / (np.array(normal_sigmas) ** 2) * 10
+        :param h5: input h5 file
+        :return: circular standard deviation score
+        """
+        H = tables.open_file(h5)
 
-    plt.plot(x, values)
-    plt.plot([0, 100], [optimal_score, optimal_score], color='red')
-    plt.xlim(0, 20)
-    plt.xlabel("solint (min)")
-    plt.ylabel("circstd score")
-    if title is not None:
-        plt.title(title)
-    plt.show()
+        stations = [make_utf8(s) for s in list(H.root.sol000.antenna[:]['name'])]
 
+        if not station:
+            stations_idx = [stations.index(stion) for stion in stations if
+                            ('RS' not in stion) &
+                            ('ST' not in stion) &
+                            ('CS' not in stion) &
+                            ('DE' not in stion)]
+        else:
+            stations_idx = [stations.index(station)]
 
-def circvar_to_normvar(circ_var):
-    """
-    Convert circular variance to normal variance
+        axes = str(H.root.sol000.phase000.val.attrs["AXES"]).replace("b'", '').replace("'", '').split(',')
+        axes_idx = sorted({ax: axes.index(ax) for ax in axes}.items(), key=lambda x: x[1], reverse=True)
 
-    return: circular variance
-    """
-    #     return -2*np.log(1-circ_var/np.pi)
-    return (1 - np.exp(-circ_var / 2))
+        phase = H.root.sol000.phase000.val[:] * H.root.sol000.phase000.weight[:]
+        H.close()
 
+        phasemod = phase % (2 * np.pi)
 
-def get_C(phasediff):
-    """
-    Get constant defining the normal circular distribution
+        for ax in axes_idx:
+            if ax[0] == 'pol':  # YX should be zero
+                phasemod = phasemod.take(indices=0, axis=ax[1])
+            elif ax[0] == 'dir':  # there should just be one direction
+                if phasemod.shape[ax[1]] == 1:
+                    phasemod = phasemod.take(indices=0, axis=ax[1])
+                else:
+                    sys.exit('ERROR: This solution file should only contain one direction, but it has ' +
+                             str(phasemod.shape[ax[1]]) + ' directions')
+            elif ax[0] == 'freq':  # faraday corrected
+                phasemod = np.diff(phasemod, axis=ax[1])
+            elif ax[0] == 'ant':  # take only international stations
+                phasemod = phasemod.take(indices=stations_idx, axis=ax[1])
 
-    See:
-    - https://en.wikipedia.org/wiki/Wrapped_normal_distribution
-    - https://en.wikipedia.org/wiki/Yamartino_method
-    - https://en.wikipedia.org/wiki/Directional_statistics
+        phasemod[phasemod == 0] = np.nan
 
+        return circstd(phasemod, nan_policy='omit')
 
-    :param phasediff: phase difference between XX and YY
+    def optimal_solint(self):
+        """
+        Get optimal solution interval from phasediff, given C
 
-    :return: C
-    """
-    circvar = get_circvar(phasediff)
-    normvar = circvar_to_normvar(circvar)
-    solint = 1  # by setting it to 1, the function returns how many times solution interval increase
-    return normvar * solint
-
-
-def get_optimal_solint(phasediff, C):
-    """
-    Get optimal solution interval from phasediff, given C
-
-    :param phasediff: phase difference between XX and YY
-    :param solint: solution interval
-
-    :return: value corresponding with increase solution interval
-    """
-    C = get_C(phasediff)
-    optimal_cirvar = optimal_score ** 2
-    return C / (circvar_to_normvar(optimal_cirvar)) / 2
-
+        :return: value corresponding with increase solution interval
+        """
+        self.cstd = self.get_phasediff_score(self.h5)
+        self.C = self._get_C(self.cstd)
+        optimal_cirvar = self.optimal_score ** 2
+        return self.C / (self._circvar_to_normvar(optimal_cirvar)) * self.ref_solint
 
 if __name__ == "__main__":
 
-    # random data
-    x = np.cos(normal(0, 1.7, 100000))
-    y = np.sin(normal(0, 1.6, 100000))
-    data = np.angle(x + y * 1j)
+    # set std score, for which you want to find the solint
+    optimal_score = 0.5
+
+    # reference solution interval
+    ref_solint = 2
+
+    h5 = '../bad2min.h5'
+
+    S = GetSolint(h5, optimal_score, ref_solint)
+    solint = S.optimal_solint()
 
     # find C
-    C = get_C(data)
-    print("C=" + str(round(C, 3)) + " for this distribution")
-    plot_C(C, "T=" + str(round(get_optimal_solint(data, C), 2)) + " min")
+    S.plot_C("T=" + str(round(solint, 2)) + " min")
