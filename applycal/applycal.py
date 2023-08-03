93dcdaeed33075866f8a4282f0429ba7ac832355
jurjen93 <jurjendejong93@gmail.com>
2023-08-03 15:03:06 +0200

    t

--- applycal/applycal.py
+++ applycal/applycal.py
@@ -30,7 +30,7 @@ class ApplyCal:
         steps.append('beam_dir')
         T = tables.open_file(h5)
         dir=T.root.sol000.source[:]['dir'][0]*360/2/pi # convert to degree
-        self.cmd += [f'beam.type=applybeam beam.direction=[{dir[0]}deg,{dir[1]}deg]']
+        self.cmd += [f'beam_dir.type=applybeam beam_dir.direction=[{dir[0]}deg,{dir[1]}deg]']
         T.close()
 
         if self.isfulljones(h5):
