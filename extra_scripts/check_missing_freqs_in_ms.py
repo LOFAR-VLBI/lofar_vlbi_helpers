9c34e17a42d3ab92f3d4a9b93d6dc1b63bffeee4
jurjen93 <jurjendejong93@gmail.com>
2023-08-07 10:30:02 +0200

    upd

--- extra_scripts/check_missing_freqs_in_ms.py
+++ extra_scripts/check_missing_freqs_in_ms.py
@@ -12,15 +12,25 @@ import numpy as np
 import sys
 from glob import glob
 
-def get_channels(input):
+def get_channels(input_ms):
     """
     :param input: list or string with measurement sets
     :return: sorted concatenated channels
     """
-    if type(input) == str:
-        input = sorted(glob(input))
 
-    for n, ms in enumerate(input):
+    # if string, make list and sort on name (so give clever names if you want to sort on freq)
+    if type(input_ms) == str:
+        input_ms = sorted(glob(input_ms))
+    elif type(input_ms) == list:
+        input_ms = sorted(input_ms)
+    else:
+        sys.exit("ERROR: wrong input type for "+str(input_ms)+" (only accept strings or lists).")
+
+    if len(input_ms) == 0:
+        sys.exit("ERROR: no MS given.")
+
+    # collect all frequencies in 1 numpy array
+    for n, ms in enumerate(input_ms):
         t = ct.table(ms + '/::SPECTRAL_WINDOW')
 
         if n == 0:
@@ -30,16 +40,21 @@ def get_channels(input):
 
         t.close()
 
-    return np.sort(chans), sorted(input)
+    return np.sort(chans), input_ms
 
 
-def check_channels(input, make_dummies, output_name):
+def has_freq_gaps(input, make_dummies, output_name):
     """
     :param input: list or string with measurement sets
-    :return: True if no error, otherwise sys.exit
+    :return: True if nog gaps, False if gaps
     """
+
+    # get frequency channels
     chans, mslist = get_channels(input)
+    # get differences between frequency steps
     chan_diff = np.abs(np.diff(chans, n=2))
+
+    # file to write sorted list of MS
     file = open(output_name, 'w')
 
     # check gaps in freqs
@@ -73,5 +88,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if check_channels(input=args.ms, make_dummies=args.make_dummies, output_name=args.output_name):
+    if has_freq_gaps(input=args.ms, make_dummies=args.make_dummies, output_name=args.output_name):
         print('--- SUCCESS: no frequency gaps found ---')
