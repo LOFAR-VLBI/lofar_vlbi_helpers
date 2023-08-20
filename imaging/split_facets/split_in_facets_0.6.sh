39dea98a2eb2b5baf3b430c70ffcc9c08bb717b7
jurjen93 <jurjendejong93@gmail.com>
2023-08-20 16:54:54 +0200

    polrot

--- imaging/split_facets/split_in_facets_0.6.sh
+++ imaging/split_facets/split_in_facets_0.6.sh
@@ -6,19 +6,8 @@ SING_BIND=$( python3 $HOME/parse_settings.py --BIND )
 SIMG=$( python3 $HOME/parse_settings.py --SIMG )
 SCRIPT_DIR=/home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/imaging/split_facets
 
-#echo "COPY DATA"
-#SOURCEDIR=/project/lofarvwf/Share/jdejong/output/ELAIS/ALL_L/apply_delaycal
-#MAX_PARALLEL=8
-#nroffiles=$(ls -1d $SOURCEDIR/*.ms|wc -w)
-#setsize=$(( nroffiles/MAX_PARALLEL + 1 ))
-#ls -1d $SOURCEDIR/*.ms | xargs -n $setsize | while read workset; do
-#  echo "COPY $workset"
-#  cp -r $workset .
-#done
-#wait
-
 echo "COPY SOLUTION FILES"
-cp /project/lofarvwf/Share/jdejong/output/ELAIS/ALL_L/dd_solutions/merged_L??????_norm.h5 . #TODO: pol?
+cp /project/lofarvwf/Share/jdejong/output/ELAIS/ALL_L/dd_solutions/merged_L??????_polrot.h5 . #TODO: pol?
 
 LISTMS=(/project/lofarvwf/Share/jdejong/output/ELAIS/ALL_L/apply_delaycal/*L68*.ms)
 H5S=(*L68*.h5)
