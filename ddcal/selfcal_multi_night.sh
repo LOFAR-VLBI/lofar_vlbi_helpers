bc6587fb4704f01b2e9cddd08ea6cfbbea455555
jurjen93 <jurjendejong93@gmail.com>
2023-09-18 12:00:09 +0200

    upd

--- ddcal/selfcal_multi_night.sh
+++ ddcal/selfcal_multi_night.sh
@@ -10,6 +10,7 @@ DIR=$1
 SIMG=$( python3 $HOME/parse_settings.py --SIMG )
 BIND=$( python3 $HOME/parse_settings.py --BIND )
 echo "SINGULARITY IS $SIMG"
+
 #SCRIPTS
 lofar_facet_selfcal=$( python3 $HOME/parse_settings.py --facet_selfcal )
 
@@ -23,6 +24,7 @@ python $lofar_facet_selfcal \
 --targetcalILT='scalarphase' \
 --stop=12 \
 --flagtimesmeared \
+--makeimage-ILTlowres-HBA \
 --makeimage-fullpol \
 --helperscriptspath=/home/lofarvwf-jdejong/scripts/lofar_facet_selfcal \
 --helperscriptspathh5merge=/home/lofarvwf-jdejong/scripts/lofar_helpers \
