d17b306fa4620557d01785bc2829824c78381649
jurjen93 <jurjendejong93@gmail.com>
2023-09-20 16:48:10 +0200

    t

--- _pipeline_scripts/download_lta.sh
+++ _pipeline_scripts/download_lta.sh
@@ -44,32 +44,35 @@ mkdir -p $LNUM
 cd $LNUM
 
 #CALIBRATOR
-if [ "$SLURM_ARRAY_TASK_ID" -eq 0 ]; then
+if [[ "$SLURM_ARRAY_TASK_ID" -eq 0 ]]; then
   LNUM_CAL=$( grep -Po 'L[0-9][0-9][0-9][0-9][0-9][0-9]' $CALDAT | head -n 1 )
   echo "DOWNLOAD DATA FOR $LNUM_CAL"
   TYPE=calibrator
+  TARS=$CALDAT
 fi
 
 #TARGET
-if [ "$SLURM_ARRAY_TASK_ID" -eq 1 ]; then
+if [[ "$SLURM_ARRAY_TASK_ID" -eq 1 ]]; then
   echo "DOWNLOAD DATA FOR $LNUM"
   TYPE=target
+  TARS=$TARDAT
 fi
 
-mkdir -p $TYPE
+mkdir -p $TYPE/Data
+cd $TYPE/Data
 
 #download data
-wget -ci $CALDAT -P $TYPE
+wget -ci $TARS
 
 # untar data
-for TAR in $TYPE/*.tar; do
+for TAR in *.tar; do
   tar -xvf $TAR
   rm -r $TAR
 done
 mv $TYPE/*.MS $TYPE/Data
 
 # find missing data
-python3 $SCRIPT_DIR/download_scripts/findmissingdata.py --path $TYPE/Data
+python3 $SCRIPT_DIR/download_scripts/findmissingdata.py
 
 # remove bands above 168 MHz
-singularity exec -B ${BIND} ${SIMG} python $SCRIPT_DIR/download_scripts/removebands.py --datafolder $TYPE/Data
+singularity exec -B ${BIND} ${SIMG} python $SCRIPT_DIR/download_scripts/removebands.py --freqcut 168
