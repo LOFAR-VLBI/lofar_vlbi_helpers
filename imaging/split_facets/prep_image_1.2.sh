0ff22fe2332dab56a296a3e44e8428b6b0e14497
jurjen93 <jurjendejong93@gmail.com>
2023-11-20 17:14:27 +0100

    t

--- imaging/split_facets/prep_image_1.2.sh
+++ imaging/split_facets/prep_image_1.2.sh
@@ -8,15 +8,17 @@
 FACET=facet_$1
 
 cd ${FACET}
-mkdir -p 06imaging
-cd 06imaging
+mkdir -p 1.2imaging
+cd 1.2imaging
 cp ../../poly_$1.reg .
 
 for MS in ../imaging/*.ms; do
   echo $MS
   singularity exec -B /project/lofarvwf/Share/jdejong/,$PWD /project/lofarvwf/Software/singularity/lofar_sksp_v4.4.0_znver2_znver2_aocl4.sif \
-  DP3 msin=$MS msout=avg0.6_${MS##*/} msin.datacolumn=DATA msout.storagemanager=dysco msout.writefullresflag=False steps=[avg] avg.type=averager avg.freqstep=2 avg.timestep=2
+  DP3 msin=$MS msout=avg1.2_${MS##*/} msin.datacolumn=DATA msout.storagemanager=dysco msout.writefullresflag=False steps=[avg] avg.type=averager avg.freqstep=4 avg.timestep=4
 done
 
+#sbatch /home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/imaging/split_facets/make_image_1.2.sh $1
+
 singularity exec -B /project/lofarvwf/Share/jdejong/,$PWD /project/lofarvwf/Software/singularity/lofar_sksp_v4.4.0_znver2_znver2_aocl4.sif \
-python /home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/imaging/split_facets/make_image.py --resolution 0.6 --facet $1 --facet_info ../../polygon_info.csv --tmpdir
+python /home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/imaging/split_facets/make_image.py --resolution 1.2 --facet $1 --facet_info ../../polygon_info.csv --tmpdir
