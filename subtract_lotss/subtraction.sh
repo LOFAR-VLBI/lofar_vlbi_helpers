f89e7d0d911f1f9e6661659225a4ffebdfd76630
jurjen93 <jurjendejong93@gmail.com>
2024-01-23 10:01:55 +0100

    time

--- subtract_lotss/subtraction.sh
+++ subtract_lotss/subtraction.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -N 1 -c 24 --job-name=subtract --exclusive --constraint="rome|intel"
+#SBATCH -N 1 -c 24 --job-name=subtract --exclusive --constraint="rome|intel" -t 36:00:00
 
 echo "Job landed on $(hostname)"
 
