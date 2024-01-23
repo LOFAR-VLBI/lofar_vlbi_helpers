c3c3eb1360203c8f9ce13486bd9661d5aaa892e2
jurjen93 <jurjendejong93@gmail.com>
2024-01-23 09:41:04 +0100

    t

--- subtract_lotss/subtraction.sh
+++ subtract_lotss/subtraction.sh
@@ -6,10 +6,10 @@ echo "Job landed on $(hostname)"
 MS=$1
 
 SIMG=/project/lofarvwf/Software/singularity/lofar_sksp_v3.3.4_x86-64_generic_avx512_ddfpublic.sif
-SING_BIND=$( python $HOME/parse_settings.py --BIND )
+SING_BIND=$( python3 $HOME/parse_settings.py --BIND )
 echo "SINGULARITY IS $SIMG"
 
-singularity exec -B $SING_BIND $SIMG /project/lofarvwf/Software/lofar_facet_selfcal/sub-sources-outside-region.py \
+singularity exec -B $SING_BIND $SIMG python /project/lofarvwf/Software/lofar_facet_selfcal/sub-sources-outside-region.py \
 --boxfile boxfile.reg \
 --column DATA_DI_CORRECTED \
 --freqavg 1 \
