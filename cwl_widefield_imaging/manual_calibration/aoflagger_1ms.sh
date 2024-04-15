ffe2bad85349e66e7d6f54bd2785543681b17ef6
jurjen93 <jurjendejong93@gmail.com>
2024-04-15 18:33:47 +0200

    aoflagger

--- cwl_widefield_imaging/manual_calibration/aoflagger_1ms.sh
+++ cwl_widefield_imaging/manual_calibration/aoflagger_1ms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -c 25 --job-name=aoflagger --constraint=amd -t 10:00:00
+#SBATCH -c 45 --job-name=aoflagger --constraint=amd -t 10:00:00
 
 SIMG=$( python3 $HOME/parse_settings.py --SIMG )
 SING_BIND=$( python3 $HOME/parse_settings.py --BIND )
