c70dc248b4d4a344f78cd9af9613218713c285a5
jurjen93 <jurjendejong93@gmail.com>
2024-04-15 10:26:33 +0200

    flagging

--- cwl_widefield_imaging/manual_subtract/concat.sh
+++ cwl_widefield_imaging/manual_subtract/concat.sh
@@ -9,7 +9,7 @@ if [[ $PWD =~ $re ]]; then OBSERVATION=${BASH_REMATCH}; fi
 
 singularity exec -B $SING_BIND $SIMG python /project/lofarvwf/Software/lofar_helpers/ms_helpers/concat_with_dummies.py \
 --msin sub6asec*.ms \
---time_res 4 \
+--time_avg 4 \
 --freq_res '97656.25Hz' \
 --phase_center '[16h06m07.61855,55d21m35.4166]' \
 --msout ${OBSERVATION}_DI.concat.ms
