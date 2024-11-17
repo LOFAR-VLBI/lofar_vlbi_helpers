8a51c3e9371e336fe4987a944a845bb9ad690c30
jurjen93 <jurjendejong93@gmail.com>
2024-11-17 10:20:34 +0100

    delycal

--- elais_128h/run_delaycal.sh
+++ elais_128h/run_delaycal.sh
@@ -6,7 +6,7 @@
 #### UPDATE THESE ####
 ######################
 
-export TOIL_SLURM_ARGS="--export=ALL --job-name delaycal -p normal"
+export TOIL_SLURM_ARGS="--export=ALL -p normal"
 
 SING_BIND="/project,/project/lofarvwf/Software,/project/lofarvwf/Share,/project/lofarvwf/Public,/home/lofarvwf-jdejong"
 DELAYCAL=/project/lofarvwf/Share/jdejong/output/ELAIS/delaycalibrator.csv
@@ -33,6 +33,8 @@ git clone https://github.com/revoltek/losoto
 mkdir scripts
 cp LINC/scripts/* scripts
 cp VLBI_cwl/scripts/* scripts
+
+# set up paths (hacky)
 SCRIPTS_PATH=$PWD/scripts
 SING_BIND=${SING_BIND}",${SCRIPTS_PATH}:/opt/lofar/DynSpecMS"
 PYPATH=${PWD}/VLBI_cwl/scripts:${PWD}/LINC/scripts:\$PYTHONPATH
@@ -70,37 +72,7 @@ export LINC_DATA_ROOT=$PWD/software/LINC
 
 ########################
 
-# PREP SOLUTIONS
-
-# convert DDF solution files to h5parm
-C=0
-for KILLMSFILE in $DDFOLDER/SOLSDIR/*MHz_uv_pre-cal.ms/*DIS2*.sols.npz; do
-  echo ${KILLMSFILE}
-  singularity exec singularity/$SIMG \
-  python software/losoto/bin/killMS2H5parm.py \
-  --solset sol000 \
-  --verbose \
-  DDF${C}.h5 \
-  ${KILLMSFILE}
-
-  ((C++))  # increment the counter
-done
-
-# merge h5parm into 1 file
-singularity exec singularity/$SIMG \
-python software/lofar_helpers/h5_merger.py \
---h5_tables DDF*.h5 \
---h5_out DDF_merged.h5 \
---propagate_flags \
---add_ms_stations \
---ms $( ls $TARGETDATA/*.MS -1d | head -n 1) \
---merge_diff_freq \
---h5_time_freq true
-
-########################
-
-# MAKE CONFIG FILE
-
+# MAKE JSON CONFIG FILE
 singularity exec singularity/$SIMG \
 python software/flocs/runners/create_ms_list.py \
 VLBI \
@@ -112,26 +84,27 @@ delay-calibration \
 --delay_calibrator=$DELAYCAL \
 --linc=$PWD/software/LINC \
 --ddf_solsdir=$DDFOLDER/SOLSDIR \
+--ddf_rundir=$DDFOLDER \
 $TARGETDATA
 
 # update json
-jq --arg nv "$PWD/DDF_merged.h5" '. + {"ddf_solset": {"class": "File", "path": $nv}}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
-jq --arg nv "$DDFOLDER" '. + {"ddf_rundir": {"class": "Directory", "path": $nv}}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
-jq --arg nv "$DDFOLDER/SOLSDIR" '. + {"ddf_solsdir": {"class": "Directory", "path": $nv}}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
+#jq --arg nv "$DDFOLDER" '. + {"ddf_rundir": {"class": "Directory", "path": $nv}}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
+#jq --arg nv "$DDFOLDER/SOLSDIR" '. + {"ddf_solsdir": {"class": "Directory", "path": $nv}}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
 jq '. + {"subtract_lotss_model": true}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
 jq '. + {"ms_suffix": ".MS"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
+jq '. + {"phasesol": "TGSSphase_final"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
 
 source ${VENV}/bin/activate
 
-TGSSphase_final_lines=$(singularity exec singularity/$SIMG python software/lofar_helpers/h5_merger.py -in=$SOLSET | grep "TGSSphase_final" | wc -l)
-# Check if the line count is greater than 1
-if [ "$TGSSphase_final_lines" -ge 1 ]; then
-    echo "Use TGSSphase_final"
-    jq '. + {"phasesol": "TGSSphase_final"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
-else
-    echo "Use TGSSphase"
-    jq '. + {"phasesol": "TGSSphase"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
-fi
+#TGSSphase_final_lines=$(singularity exec singularity/$SIMG python software/lofar_helpers/h5_merger.py -in=$SOLSET | grep "TGSSphase_final" | wc -l)
+## Check if the line count is greater than 1
+#if [ "$TGSSphase_final_lines" -ge 1 ]; then
+#    echo "Use TGSSphase_final"
+#    jq '. + {"phasesol": "TGSSphase_final"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
+#else
+#    echo "Use TGSSphase"
+#    jq '. + {"phasesol": "TGSSphase"}' mslist_VLBI_delay_calibration.json > temp.json && mv temp.json mslist_VLBI_delay_calibration.json
+#fi
 
 ########################
 
@@ -144,7 +117,6 @@ JOBSTORE=$PWD/jobstore
 LOGDIR=$PWD/logs
 TMPD=$PWD/tmpdir
 
-mkdir -p ${TMPD}_interm
 mkdir -p $WORKDIR
 mkdir -p $OUTPUT
 mkdir -p $LOGDIR
@@ -167,7 +139,6 @@ toil-cwl-runner \
 --jobStore ${JOBSTORE} \
 --workDir ${WORKDIR} \
 --coordinationDir ${OUTPUT} \
---tmpdir-prefix ${TMPD}_interm/ \
 --disableAutoDeployment True \
 --bypass-file-store \
 --preserve-entire-environment \
