027efb6310dee9df0787adc969ff90e2e7a95ae2
James Petley <jwpetley@gmail.com>
2025-01-06 11:23:14 +0100

    Selfcal in LINC

--- elais_128h/run_linc.sh
+++ elais_128h/run_linc.sh
@@ -3,13 +3,13 @@
 #SBATCH --output=linc_%j.out
 #SBATCH --error=linc_%j.err
 
-FLOCSRUNNERS=/project/lofarvwf/Software/flocs/runners
+FLOCSRUNNERS=/project/wfedfn/Software/flocs/runners
 
 STARTDIR=$PWD
 
 #SINGULARITY SETTINGS
 SING_BIND=$( python3 $HOME/parse_settings.py --BIND )
-SIMG=/project/lofarvwf/Software/singularity/flocs_v5.2.0_znver2_znver2.sif
+SIMG=/project/wfedfn/Software/singularity/flocs_v5.3.0_znver2_znver2.sif
 
 #GET ORIGINAL SCRIPT DIRECTORY
 if [ -n "${SLURM_JOB_ID:-}" ] ; then
@@ -52,6 +52,6 @@ fi
 singularity exec -B ${SING_BIND} ${SIMG} python ~/scripts/lofar_vlbi_helpers/elais_128h/download_scripts/removebands.py --freqcut 168 --datafolder data
 
 # Run LINC target
-singularity exec -B ${SING_BIND} ${SIMG} $FLOCSRUNNERS/run_LINC_target_HBA.sh -d $STARTDIR/target/data -c $STARTDIR/calibrator/*_LINC_calibrator/results_LINC_calibrator/cal_solutions.h5 -e "--make_structure_plot=False" -l /project/lofarvwf/Software/LINC
+singularity exec -B ${SING_BIND} ${SIMG} $FLOCSRUNNERS/run_LINC_target_HBA.sh -d $STARTDIR/target/data -c $STARTDIR/calibrator/*_LINC_calibrator/results_LINC_calibrator/cal_solutions.h5 -e "--make_structure_plot=False --selfcal=True" -l /project/wfedfn/Software/LINC
 
 cd ../
