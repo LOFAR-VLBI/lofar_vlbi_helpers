222621da6a40ff6599326003316c821196778971
James Petley <jwpetley@gmail.com>
2025-01-14 10:31:41 +0100

    Update simg image

--- elais_128h/run_ddf.sh
+++ elais_128h/run_ddf.sh
@@ -15,7 +15,7 @@ RUNDIR=$TMPDIR/ddf
 mkdir -p $RUNDIR
 mkdir -p $OUTPUT
 
-SIMG=flocs_v4.5.0_znver2_znver2_aocl4_cuda.sif
+SIMG=flocs_v5.4.0_znver2_znver2.sif
 
 cd $RUNDIR
 
--- elais_128h/run_delaycal.sh
+++ elais_128h/run_delaycal.sh
@@ -9,9 +9,9 @@
 
 export TOIL_SLURM_ARGS="--export=ALL -p normal -t 24:00:00"
 
-SING_BIND="/project,/project/lofarvwf/Software,/project/lofarvwf/Share,/project/lofarvwf/Public"
-DELAYCAL=/project/lofarvwf/Share/jdejong/output/ELAIS/delaycalibrator.csv
-CONFIG=/project/lofarvwf/Share/jdejong/output/ELAIS/delaysolve_config.txt
+SING_BIND="/project,/project/wfedfn/Software,/project/wfedfn/Share,/project/wfedfn/Public"
+DELAYCAL=/project/wfedfn/Share/jdejong/output/ELAIS/delaycalibrator.csv
+CONFIG=/project/wfedfn/Share/jdejong/output/ELAIS/delaysolve_config.txt
 
 #VENV=/project/lofarvwf/Software/venv
 
@@ -45,7 +45,7 @@ cd ../
 # set up singularity
 export SIMG=vlbi-cwl.sif
 mkdir -p singularity
-cp /project/lofarvwf/Software/singularity/flocs_v5.1.0_znver2_znver2_test.sif singularity/$SIMG
+cp /project/lofarvwf/Software/singularity/flocs_v5.4.0_znver2_znver2_test.sif singularity/$SIMG
 mkdir -p singularity/pull
 cp singularity/$SIMG singularity/pull/$SIMG
 
@@ -119,7 +119,7 @@ jq '. + {"ms_suffix": ".MS"}' mslist_VLBI_delay_calibration.json > temp.json && 
 
 #source ${VENV}/bin/activate
 
-TGSSphase_final_lines=$(singularity exec -B /project/lofarvwf singularity/$SIMG python software/lofar_helpers/h5_merger.py -in=$SOLSET | grep "TGSSphase" | wc -l)
+TGSSphase_final_lines=$(singularity exec -B /project/wfedfn singularity/$SIMG python software/lofar_helpers/h5_merger.py -in=$SOLSET | grep "TGSSphase" | wc -l)
 # Check if the line count is greater than 1
 if [ "$TGSSphase_final_lines" -ge 1 ]; then
     echo "Use TGSSphase_final"
