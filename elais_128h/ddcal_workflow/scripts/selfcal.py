b7435b3646165d6572b660489e732e663c3b3efc
jurjen93 <jurjendejong93@gmail.com>
2025-01-30 18:06:45 +0100

    less

--- elais_128h/ddcal_workflow/scripts/selfcal.py
+++ elais_128h/ddcal_workflow/scripts/selfcal.py
@@ -101,80 +101,77 @@ def make_config(solint, ms):
     # solint in minutes
     solint_scalarphase_1 = min(max(deltime/60, np.sqrt(solint)), 3)
     solint_scalarphase_2 = min(max(deltime/60, np.sqrt(1.5*solint)), 5)
-    solint_scalarphase_3 = min(max(deltime/60, np.sqrt(6*solint)), 5)
 
-    solint_complexgain_1 = max(20.0, 25*solint)
+    solint_complexgain_1 = max(20.0, 40*solint)
     solint_complexgain_2 = 1.5 * solint_complexgain_1
 
     cg_cycle_1 = 3
-    if solint_complexgain_1/60 > 8:
+    if solint_complexgain_1/60 > 6:
         cg_cycle_1 = 999
-    elif solint_complexgain_1/60 > 5:
-        solint_complexgain_1 = 480.
     elif solint_complexgain_1/60 > 3:
         solint_complexgain_1 = 240.
 
     cg_cycle_2 = 4
-    if solint_complexgain_2/60 > 5:
+    if solint_complexgain_2/60 > 6:
         cg_cycle_2 = 999
     elif solint_complexgain_2/60 > 3:
         solint_complexgain_2 = 240.
 
-    soltypecycles_list = f'[0,0,2,{cg_cycle_1},{cg_cycle_2}]'
-    smoothnessreffrequency_list = "[120.0,120.0,120.0,0.0,0.0]"
-    smoothnessspectralexponent_list = "[-1.0,-1.0,-1.0,-1.0,-1.0]"
-    antennaconstraint_list = "[None,None,'alldutch',None,None]"
-    soltype_list = "['scalarphase','scalarphase','scalarphase','scalarcomplexgain','scalarcomplexgain']"
-    solint_list = f"['{int(solint_scalarphase_1 * 60)}s','{int(solint_scalarphase_2 * 60)}s','{int(solint_scalarphase_3 * 60)}s','{int(solint_complexgain_1*60)}s','{int(solint_complexgain_2*60)}s']"
+    soltypecycles_list = f'[0,0,{cg_cycle_1},{cg_cycle_2}]'
+    smoothnessreffrequency_list = "[120.0,120.0,0.0,0.0]"
+    smoothnessspectralexponent_list = "[-1.0,-1.0,-1.0,-1.0]"
+    antennaconstraint_list = "[None,None,None,None]"
+    soltype_list = "['scalarphase','scalarphase','scalarcomplexgain','scalarcomplexgain']"
+    solint_list = f"['{int(solint_scalarphase_1 * 60)}s','{int(solint_scalarphase_2 * 60)}s','{int(solint_complexgain_1*60)}s','{int(solint_complexgain_2*60)}s']"
     stop = 10
     imsize = 2048
     avgstep = 1
 
 
     if 'ILTJ161212.29+552303.8' in ms: #TODO: SPECIAL CASE!
-        solint_list = f"['{int(solint_scalarphase_1 * 60)}s','{int(solint_scalarphase_2 * 60)}s','{int(solint_scalarphase_3 * 60)}s','900s','{int(solint_complexgain_2 * 60)}s']"
+        solint_list = f"['{int(solint_scalarphase_1 * 60)}s','{int(solint_scalarphase_2 * 60)}s','900s','{int(solint_complexgain_2 * 60)}s']"
         uvmin=50000
         smoothness_phase = 10.0
         smoothness_complex = 10.0
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.5},{smoothness_phase*1.5},{smoothness_complex},{smoothness_complex+5.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutch','coreandallbutmostdistantremotes']"
-        antennaconstraint_list = "[None,None,'alldutch',None,None]"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.5},{smoothness_complex},{smoothness_complex+5.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutch','coreandallbutmostdistantremotes']"
+        antennaconstraint_list = "[None,None,None,None]"
         stop = 20
 
     elif solint<0.05:
         uvmin=40000
         smoothness_phase = 8.0
         smoothness_complex = 10.0
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.5},{smoothness_phase*1.5},{smoothness_complex},{smoothness_complex+5.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutch','coreandfirstremotes']"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.5},{smoothness_complex},{smoothness_complex+5.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutch','coreandfirstremotes']"
 
     elif solint<0.1:
         uvmin=35000
         smoothness_phase = 10.0
         smoothness_complex = 10.0
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutch','coreandallbutmostdistantremotes']"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutch','coreandallbutmostdistantremotes']"
 
     elif solint<1:
         uvmin=30000
         smoothness_phase = 10.0
         smoothness_complex = 12.5
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutch','coreandallbutmostdistantremotes']"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutch','coreandallbutmostdistantremotes']"
 
     elif solint<2.5:
         uvmin=25000
         smoothness_phase = 10.0
         smoothness_complex = 15.0
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+10.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutch','coreandallbutmostdistantremotes']"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+10.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutch','coreandallbutmostdistantremotes']"
 
     elif solint<4:
         uvmin=25000
         smoothness_phase = 10.0
         smoothness_complex = 20.0
-        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
-        resetsols_list = "['alldutchandclosegerman','alldutch',None,'alldutchandclosegerman','alldutch']"
+        smoothnessconstraint_list = f"[{smoothness_phase},{smoothness_phase*1.25},{smoothness_complex},{smoothness_complex+5.0}]"
+        resetsols_list = "['alldutchandclosegerman','alldutch','alldutchandclosegerman','alldutch']"
 
     elif solint<15:
         uvmin=20000
