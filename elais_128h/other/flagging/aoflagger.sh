d608c33cf102a41c9110f56168d5cd2d2ae40333
jurjen93 <jurjendejong93@gmail.com>
2025-03-19 16:38:11 +0100

    flag

--- elais_128h/other/flagging/aoflagger.sh
+++ elais_128h/other/flagging/aoflagger.sh
@@ -4,18 +4,8 @@
 SIMG=$( python3 $HOME/parse_settings.py --SIMG )
 SING_BIND=$( python3 $HOME/parse_settings.py --BIND )
 
-pattern="sub6asec*.ms"
+pattern="*.ms"
 MS_FILES=( $pattern )
-SB=${MS_FILES[${SLURM_ARRAY_TASK_ID}]}
+MS=${MS_FILES[${SLURM_ARRAY_TASK_ID}]}
 
-RUNDIR=$TMPDIR/subtract_${SLURM_ARRAY_TASK_ID}
-OUTDIR=$PWD
-
-mkdir -p $RUNDIR
-cp -r $SB $RUNDIR
-cp $SIMG $RUNDIR
-cd $RUNDIR
-
-singularity exec -B $PWD ${SIMG##*/} aoflagger ${SB##*/}
-
-mv ${SB##*/} $OUTDIR/flagged_${SB##*/}
+singularity exec -B $PWD ${SIMG} aoflagger ${MS}
