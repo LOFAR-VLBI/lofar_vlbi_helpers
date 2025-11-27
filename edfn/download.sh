f9219b3b042795fef6c48e39f1f1fc4a163354c0
jurjen93 <jurjendejong93@gmail.com>
2025-11-27 16:17:12 +0100

    edfn upd

--- edfn/download.sh
+++ edfn/download.sh
@@ -5,8 +5,6 @@ STAGE_ID=$1
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 source $SCRIPT_DIR/setup.sh --no-git --no-sing
 
-mkdir -p L${SASID}
-
-cd $MASTERDIR/L${SASID}
+cd $MASTERDIR/L${STAGE_ID}
 
 singularity exec $SING_IMG python $SOFTWARE_DIR/flocs-lta/flocs_lta/flocs_download.py --parallel-downloads 10 $STAGE_ID
