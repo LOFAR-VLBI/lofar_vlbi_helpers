a6dc75f962d3aff520346ddfe7c797c7da8649d1
jurjen93 <jurjendejong93@gmail.com>
2025-12-16 18:26:27 +0100

    linc runners

--- edfn/run_linc_calibrator.sh
+++ edfn/run_linc_calibrator.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -c 1
+#SBATCH -c 1 -p short -t 4:00:00
 #SBATCH --output=linc_%j.out
 #SBATCH --error=linc_%j.err
 
 SCRIPT_DIR=/home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/edfn
 
-source $SCRIPT_DIR/setup.sh --no-git
+source $SCRIPT_DIR/setup.sh --no-git --no-sing
 
 ulimit -S -n 8192
 
-# Ensure < 168 MHz
-singularity exec $SIMG_CACHE_DIR/$SIMG python ~/scripts/lofar_vlbi_helpers/elais_200h/download_scripts/removebands.py --freqcut 168 --datafolder data
+singularity exec $SIMG_CACHE_DIR/${SIMG}.sif python ~/scripts/lofar_vlbi_helpers/elais_200h/download_scripts/removebands.py --freqcut 168 --datafolder data
 
+# Run LINC calibrator
 source ${VENV}/bin/activate
-flocs-run linc calibrator --slurm-time "1:00:00" --slurm-queue "normal" --slurm-account lofarvwf --runner toil --scheduler slurm data
+flocs-run linc calibrator --slurm-time "48:00:00" --slurm-queue "normal" --slurm-account lofarvwf --runner cwltool --scheduler slurm data
 deactivate
--- edfn/run_linc_target.sh
+++ edfn/run_linc_target.sh
@@ -5,6 +5,8 @@
 
 SCRIPT_DIR=/home/lofarvwf-jdejong/scripts/lofar_vlbi_helpers/edfn
 
+CAL_SOLUTIONS=$(abspath $1)
+
 source $SCRIPT_DIR/setup.sh --no-git
 
 ulimit -S -n 8192
@@ -13,5 +15,5 @@ ulimit -S -n 8192
 singularity exec $SIMG_CACHE_DIR/$SIMG python ~/scripts/lofar_vlbi_helpers/elais_200h/download_scripts/removebands.py --freqcut 168 --datafolder data
 
 source ${VENV}/bin/activate
-flocs-run linc target --slurm-time "48:00:00" --slurm-queue "normal" --slurm-account lofarvwf --runner toil --scheduler slurm --cal_solutions  data
+flocs-run linc target --slurm-time "48:00:00" --slurm-queue "normal" --slurm-account lofarvwf --runner toil --scheduler slurm --cal_solutions ${CAL_SOLUTIONS} --output-fullres-data data
 deactivate
